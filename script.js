dbc0cff33d0991a98895581d203807874dc42c01
Molnár Bence <mbbexonait@gmail.com>
2025-11-14 19:08:30 +0100

    CR practice

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-// --- Tömb
-let emberek = []; 
+// --- Tomb
+const emberek = [];
 
-// --- DOM CACHING
-
+// --- DOM cache
 const nevInput = document.getElementById("nev");
 const korInput = document.getElementById("kor");
 const szakmaInput = document.getElementById("szakma");
 const berInput = document.getElementById("ber");
 
-// --- Függvények
+// --- Fuggvenyek
 
-function urites() {
+function hiba(input, message){
+    input.value = "";
+    input.classList.add("error");
+    input.placeholder = "";
+};
+
+function kiurites(){
     nevInput.value = "";
     korInput.value = "";
     szakmaInput.value = "";
@@ -28,72 +33,63 @@ function urites() {
 }
 
 function kiir(){
-    const torzs = document.getElementById("torzs");
-    torzs.innerHTML = "";
+  const torzs = document.getElementById("torzs");
+  torzs.innerHTML = "";
 
-    emberek.forEach(function(ember){
-        const tr = document.createElement("tr");
+  emberek.forEach(function(ember){
+    let tr = document.createElement("tr");
 
-        const td1 = document.createElement("td");
-        const td2 = document.createElement("td");
-        const td3 = document.createElement("td");
-        const td4 = document.createElement("td");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
 
-        td1.textContent = ember.nev;
-        td2.textContent = ember.kor;
-        td3.textContent = ember.szakma;
-        td4.textContent = ember.ber;
+    td1.textContent = ember.nev;
+    td2.textContent = ember.kor;
+    td3.textContent = ember.szakma;
+    td4.textContent = ember.ber;
 
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        tr.appendChild(td4);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
 
-        torzs.appendChild(tr);
-    })
+    torzs.appendChild(tr);
+  })
+
 }
 
-function hiba(input, message){
-    input.value = "";
-    input.classList.add("error");
-    input.placeholder = message;
-}
- // --- click
 document.getElementById("kuldes").addEventListener("click", function(){
-    const nev = nevInput.value.trim();
-    const kor = Number(korInput.value);
-    const szakma = szakmaInput.value.trim();
-    const ber = Number(berInput.value);
+      let _nev = nevInput.value.trim();
+    let _kor = Number(korInput.value);
+    let _szakma = szakmaInput.value.trim();
+    let _ber = Number(berInput.value);
 
-    if(!nev){
-        hiba(nevInput, "Add meg a nevet!");
+    if(!_nev){
+        hiba(nevInput,"Add meg a neved!");
         return;
     }
-
-    if(isNaN(kor) || kor < 18 || kor > 70){
-        hiba(korInput, "18-70 közötti kort adj meg!");
+    if(isNaN(_kor) || _kor < 18 || _kor > 70){
+        hiba(korInput, "min:18 - max:70");
         return;
     }
-
-    if(!szakma){
-        hiba(szakmaInput, "Kérlek add meg a szakmádat!");
+    if(!_szakma){
+        hiba(szakmaInput,"Add meg a szakmad!");
         return;
     }
-
-    if(isNaN(ber) || ber < 100000 || ber > 3000000){
-        hiba(berInput, "Kérlek 100.000ft és 3.000.000ft összeget adj meg!");
+    if(isNaN(_ber) || _ber < 100000 || _ber > 3000000){
+        hiba(berInput, "min:100k - max:3m!");
         return;
     }
 
     const ujEmber = {
         id: Date.now(),
-        nev: nev,
-        kor: kor,
-        szakma: szakma,
-        ber: ber
-    }
-    emberek.push(ujEmber);
-    kiir()
-    urites();
-
-});
+        nev:_nev,
+        kor:_kor,
+        szakma:_szakma,
+        ber:_ber}
+    
+        emberek.push(ujEmber);
+        kiir();
+        kiurites();
+    });
